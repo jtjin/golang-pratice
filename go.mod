18466e638b2c5b45b74f30e3f8a690e2e0ec96eb
jtjin <wade4515x@gmail.com>
2021-02-06 12:21:19 +0800

    Get start

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jtjin/golang-pratice
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ service.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jtjin/golang-pratice/controller"
+	"github.com/jtjin/golang-pratice/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+	// server.GET("/test", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{
+	// 		"message": "OK",
+	// 	})
+	// })
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+	server.Run(":8080")
+}
