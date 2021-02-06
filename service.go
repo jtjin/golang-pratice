47b3373d50d59d69f22997bfa1f5d1e81173ba50
jtjin <wade4515x@gmail.com>
2021-02-06 12:52:09 +0800

    Adding basic-auth and logger middlewares

--- service.go
+++ service.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jtjin/golang-pratice/controller"
+	"github.com/jtjin/golang-pratice/middlewares"
 	"github.com/jtjin/golang-pratice/service"
 )
 
@@ -12,12 +16,20 @@ var (
 )
 
 func main() {
-	server := gin.Default()
-	// server.GET("/test", func(ctx *gin.Context) {
-	// 	ctx.JSON(200, gin.H{
-	// 		"message": "OK",
-	// 	})
-	// })
+
+	// Logging to a file.
+	f, _ := os.Create("gin.log")
+	// gin.DefaultWriter = io.MultiWriter(f)
+
+	// Logging to a file AND console at the same time.
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	// server := gin.Default()
+	server := gin.New()
+	server.Use(gin.Recovery())
+	server.Use(middlewares.Logger())
+	server.Use(middlewares.BasicAuth())
+
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
